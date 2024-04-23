4ac9bfdd6e21c2fb93f8ee1fc8488e4225192855
Mehdi Maaref <mehdimaaref@live.fr>
2024-04-23 22:55:42 +0200

    [WIP] :sparkles: add Spring security login <html> page

---
+++ src/main/java/net/guides/springboot/registrationlogindemo/security/CustomerUserDetailsService.java
@@ -0,0 +1,48 @@
+package net.guides.springboot.registrationlogindemo.security;
+import net.guides.springboot.registrationlogindemo.entity.Role;
+import net.guides.springboot.registrationlogindemo.entity.User;
+import net.guides.springboot.registrationlogindemo.repository.UserRepository;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+@Service
+public class CustomerUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomerUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email);
+
+        if (user != null) {
+            return new org.springframework.security.core.userdetails.User(user.getEmail(),
+                    user.getPassword(),
+                    mapRolesToAuthorities(user.getRoles()));
+        }else{
+            throw new UsernameNotFoundException("Invalid username or password.");
+        }
+    }
+
+
+
+
+
+        private Collection < ? extends GrantedAuthority> mapRolesToAuthorities(Collection <Role> roles) {
+            return roles.stream()
+                    .map(role -> new SimpleGrantedAuthority(role.getName()))
+                    .collect(Collectors.toList());
+        }
+    }
+
